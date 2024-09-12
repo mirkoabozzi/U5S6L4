8213124ea56d62dc30ae532e0384cb564f8ac471
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-09-12 13:48:54 +0200

    Initial commit

---
+++ src/main/java/mirkoabozzi/U5S6L4/U5S6L4Application.java
@@ -0,0 +1,13 @@
+package mirkoabozzi.U5S6L4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5S6L4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5S6L4Application.class, args);
+	}
+
+}
---
+++ src/test/java/mirkoabozzi/U5S6L4/U5S6L4ApplicationTests.java
@@ -0,0 +1,13 @@
+package mirkoabozzi.U5S6L4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S6L4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
